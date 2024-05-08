7bce8ac57a9fda409772a81917eb5d4f7b89a7d0
aquispearr <aquispearr@unsa.edu.com>
2024-05-07 22:33:51 -0500

    First commit

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+class Calculator {
+	  //Alexandra
+  int add(int a, int b){ return 0; }
+ 
+ //Paul
+  int sub(int a, int b){ return 0; }
+  int mul(int a, int b){ return 0; }
+ //Joao
+  int div(int a, int b){ return 0; }
+  int mod(int a, int b){ return 0; }
+}
+
