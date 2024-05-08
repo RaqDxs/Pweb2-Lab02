e9d4ee91b93179612b17349050dd8c87f20c100b
Jeferson <jbasurco@unsa.edu.pe>
2024-05-07 22:46:28 -0500

    Métodos div y mod

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,20 @@
 class Calculator {
-	  //Alexandra
-  int add(int a, int b){ return 0; }
- 
- //Paul
-  int sub(int a, int b){ return 0; }
-  int mul(int a, int b){ return 0; }
- //Joao
-  int div(int a, int b){ return 0; }
-  int mod(int a, int b){ return 0; }
+public static void main(String [] args){
+  //Alexandra
+    int add(int a, int b){ return 0; }
+  
+    //Paul
+    int sub(int a, int b){ return 0; }
+    int mul(int a, int b){ return 0; }
+    
+    public static int div (int a, int b){
+      double d = (double)(a/b);
+      System.out.println("Resultado: "+ d);
+      return 0;
+    }
+    public static int mod (int a, int b){
+      System.out.println("Resultado: "+(a%b));
+      return 0;
+    }
+  }
 }
-
